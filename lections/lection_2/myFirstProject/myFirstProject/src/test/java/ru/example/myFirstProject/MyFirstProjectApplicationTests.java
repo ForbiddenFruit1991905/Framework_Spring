8460f85a530b2bff11011ad454bff43db5aed6a8
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-12-04 14:55:07 +0300

    first commit

---
+++ lections/lection_2/myFirstProject/myFirstProject/src/test/java/ru/example/myFirstProject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.myFirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
