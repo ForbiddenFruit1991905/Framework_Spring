542f938cfc7a3ae84ec9f4a1c3c9b3c844e43111
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-12-13 20:10:05 +0300

    add HW

--- seminars/seminar4/hw/src/main/java/ru/example/hw/controller/UserController.java
+++ seminars/seminar4/hw/src/main/java/ru/example/hw/controller/UserController.java
@@ -6,51 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.example.hw.model.User;
 import ru.example.hw.service.UserService;
 
+import java.util.UUID;
+
 
 @Controller
-//@AllArgsConstructor
+@AllArgsConstructor
 public class UserController {
 
-//    @Autowired
-//    private UserRepository userRepository;
-    @Autowired
     private UserService userService;
 
     @GetMapping("/users")
     public String getAllUsers(Model model) {
-//        List<User> users = userService.getAllUsers();
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
 
-//    @PostMapping("/users")
-//    public String userAdd(@RequestBody Map<String, Object> requestBody) {
-//        String name = (String) requestBody.get("name");
-//        Integer age = (Integer) requestBody.get("age");
-//        String email = (String) requestBody.get("email");
-//
-//        userService.addUser(name, age, email);
-//        return "User added from body!";
-//    }
-
     @PostMapping("/users")
-    public String userAdd
-//            (@RequestBody Map<String, Object> requestBody) {
-//        String name = (String) requestBody.get("name");
-//        Integer age = (Integer) requestBody.get("age");
-//        String email = (String) requestBody.get("email");
-
-            (User u, Model model) {
+    public String userAdd(@ModelAttribute("user") User u, Model model) {
+        u.setId(UUID.randomUUID());
         userService.addUser(u);
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
-
 
 }
 
--- seminars/seminar4/hw/src/main/java/ru/example/hw/model/User.java
+++ seminars/seminar4/hw/src/main/java/ru/example/hw/model/User.java
@@ -2,10 +2,15 @@ package ru.example.hw.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
 @Data
 public class User {
-
-//    private int id;
+    @Id
+    @GeneratedValue
+    private UUID id;
     private String name;
     private int age;
     private String email;
