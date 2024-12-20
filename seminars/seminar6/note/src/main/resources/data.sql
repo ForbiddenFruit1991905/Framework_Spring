28b41a2f066170c8a03ae81ec44b4238c99347db
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-12-20 12:04:09 +0300

    some fixes in application.properties, data.sql, schema.sql

--- seminars/seminar6/note/src/main/resources/data.sql
+++ seminars/seminar6/note/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-insert into notes (header, text, created_date) values ('Заказать продукты на Новый Год', 'Икра, багет, все для салатов', '2024-12-20T11:21:54.405983');
-
-insert into notes (header, text, created_date) values ('Выполнить все практические задания', 'Фреймворк Spring', '2024-12-19T11:21:54.405983');
-
-insert into notes (header, text, created_date) values ('Выбрать новогодние подарки', 'Заказать торт, праздничная одежда для близких', '2024-12-30T11:21:54.405983');
+insert into notes (header, text, created_date) values ('Заказать продукты на Новый Год', 'Икра, багет, все для салатов', '2024-12-20T11:21:54.405983')
+insert into notes (header, text, created_date) values ('Выполнить все практические задания', 'Фреймворк Spring', '2024-12-19T11:21:54.405983')
+insert into notes (header, text, created_date) values ('Выбрать новогодние подарки', 'Заказать торт, праздничная одежда для близких', '2024-12-30T11:21:54.405983')
--- seminars/seminar6/note/src/main/resources/schema.sql
+++ seminars/seminar6/note/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ create table if not exists notes
     id INT AUTO_INCREMENT primary key,
     header varchar(200) not null,
     text varchar(3000) not null,
-    created_date timestamp
+    created_date timestamp not null
 );
 
